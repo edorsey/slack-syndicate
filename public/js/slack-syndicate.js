e0959628f4bb57a7e9bd709a8033152005c8f68a
Eric Dorsey <eric@ericdorsey.com>
2014-04-21 21:32:13 -0500

    reference right server

--- public/js/slack-syndicate.js
+++ public/js/slack-syndicate.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect(window.location.href);
 var logWebhook = function(data) {
 	$('<div class="webhook-data" />').text(JSON.stringify(data)).appendTo($('body'));
 }
